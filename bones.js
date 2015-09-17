dd97dbb4005a162d1532e0b8c28abccd6639b88e
Peter Den Hartog <pddenhar@gmail.com>
2015-09-16 23:23:36 -0500

    ImageBone drawing to canvas works

--- bones.js
+++ bones.js
@@ -1,26 +1,45 @@
 (function( bones, $, undefined ) { 
   //Private Library Property 
   var map_element = null;
-
+  var matrixType = mat2d;
   //Public Property
   //bones.thing;
  
-  //Public Method
+  //Public Object Creator
   bones.bone = function(name, parent) {
-    this.parent = parent;
+    this.parent = parent || null;
     this.name = name;
-  };
- 
-  //Private Library Methods
-  function makeIterator(array){
-    var nextIndex = 0;
-    
-    return {
-      next: function(){
-        if(nextIndex >= array.length)
-          nextIndex = 0;
-        return array[nextIndex++]
-      }
+    this.transform = matrixType.create();
+
+    this.children = {};
+
+    if(this.parent) {
+      this.parent.children[this.name] = this;
     }
-  } 
+  };
+
+  bones.bone.prototype.getTransformWithParent = function() {
+    var out = matrixType.create();
+    return this.parent == null ? this.transform : matrixType.mul(out, this.transform, this.parent.getTransformWithParent())
+  }
+
+  // image - HTML5 Image() object
+  // ox, oy - bone local origin location on image (0,0 is top left)
+  bones.imageBone = function(name, parent, imageSrc, ox, oy) {
+    bones.bone.call(this, name, parent);
+    this.image = new Image(); // HTML5 Constructor
+    this.image.src = imageSrc;
+    this.ox = ox;
+    this.oy = oy;
+  }
+
+  bones.imageBone.prototype = new bones.bone()
+  bones.bone.prototype.drawToTransformedCtx = function(ctx) {
+    ctx.drawImage(this.image, -this.ox, -this.oy);
+  }
+
+  //Private Library Methods
+  // function itsPrivate(array){
+  // } 
+
 }( window.bones = window.bones || {}, null ));
