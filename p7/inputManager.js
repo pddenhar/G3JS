3d27f0c53d92243f2fa38aadc76f8a0881d9020d
Peter Den Hartog <pddenhar@gmail.com>
2015-10-21 21:12:38 -0500

    Got camera rotation working

--- p7/inputManager.js
+++ p7/inputManager.js
@@ -1,13 +1,25 @@
 (function( inputManager, $, undefined ) { 
   inputManager.keyboardState = [];
+  inputManager.mouseState = {};
+
   function initManager() {
     window.onkeydown=function(event) {
-        //console.log(event.which);
-        inputManager.keyboardState[event.which] = true;
-      };
-      window.onkeyup = function(event) {
-        inputManager.keyboardState[event.which] = false;
-      };
+      //console.log(event.which);
+      inputManager.keyboardState[event.which] = true;
+    };
+    window.onkeyup = function(event) {
+      inputManager.keyboardState[event.which] = false;
+    };
+    window.onmousedown = function(event) {
+      inputManager.mouseState[event.which] = true;
+    };
+    window.onmouseup = function(event) {
+      inputManager.mouseState[event.which] = false;
+    };
+    window.onmousemove = function(event) {
+      inputManager.mouseState.x = event.pageX;
+      inputManager.mouseState.y = event.pageY;
+    };
   }
   initManager();
 
